78ea8f2f2975751f245727bd1d50bded48a925d3
En1boy <danya.server@gmail.com>
2023-02-11 18:17:29 +0800

    добавил react skeleton и пофиксил отображение товаров в корзине

--- src/App.js
+++ src/App.js
@@ -7,74 +7,106 @@ import Home from './pages/Home';
 import Favorites from './pages/Favorites';
 
 function App() {
-  const [items, setItems] = React.useState([]);
-  const [cartItems, setCartItems] = React.useState([]);
-  const [favorites, setFavorites] = React.useState([]);
-  const [searchValue, setSearchValue] = React.useState('');
-  const [cartOpened, setCartOpened] = React.useState(false);
+	const [items, setItems] = React.useState([]);
+	const [cartItems, setCartItems] = React.useState([]);
+	const [favorites, setFavorites] = React.useState([]);
+	const [searchValue, setSearchValue] = React.useState('');
+	const [cartOpened, setCartOpened] = React.useState(false);
+	const [isLoading, setIsLoading] = React.useState(true);
 
-  React.useEffect(() => {
-    axios.get('https://634934c9a59874146b19fe11.mockapi.io/items').then((res) => {    //получение карточек с бэка (items)
-      setItems(res.data);
-    });
-    axios.get('https://634934c9a59874146b19fe11.mockapi.io/cart').then((res) => {     //получение данных(cart) с бэка в корзину
-      setCartItems(res.data);
-    });
-    axios.get('https://634934c9a59874146b19fe11.mockapi.io/favorite').then((res) => {     //получение данных(favorite) с бэка в корзину
-      setFavorites(res.data);
-  });
-  }, []);
+	React.useEffect(() => {
+		async function fetchData() {
+			const cartResponse = await axios.get(
+				'https://634934c9a59874146b19fe11.mockapi.io/cart',
+			);
+			const favoritesResponse = await axios.get(
+				'https://634934c9a59874146b19fe11.mockapi.io/favorite',
+			);
+			const itemsResponse = await axios.get(
+				'https://634934c9a59874146b19fe11.mockapi.io/items',
+			);
 
-  const onAddToCart = (obj) => {
-    axios.post('https://634934c9a59874146b19fe11.mockapi.io/cart', obj)
-    .then(res => setCartItems(prev => [...prev, res.data]))                   //занесение данных в бэкенд в корзину
-  };
+			setIsLoading(false); //состояние карточек при загрузке данных(react skeleton)
 
-  const onRemoveItem = (id) => {
-    axios.delete(`https://634934c9a59874146b19fe11.mockapi.io/cart/${id}`);  //удаление данных в бэкенд в корзине
-    setCartItems((prev) => prev.filter(item => item.id !== id));
-  };
+			setCartItems(cartResponse.data);
+			setFavorites(favoritesResponse.data);
+			setItems(itemsResponse.data);
+		}
 
-  const onAddToFavorite = async (obj) => {
-    try {
-      if (favorites.find((favObj) => favObj.id === obj.id)){
-        axios.delete(`https://634934c9a59874146b19fe11.mockapi.io/favorite/${obj.id}`);
-        setFavorites(prev => prev.filter(item => item.id !== obj.id));
-      } else {
-        const { data } = await axios.post('https://634934c9a59874146b19fe11.mockapi.io/favorite', obj);  //занесение данных в бэкенд в закладки
-        setFavorites(prev => [...prev, data]);
-      }
-    } catch (error) {
-      alert('Не удалось добавить в закладки');
-    }
-  };
+		fetchData();
+	}, []);
 
-  const onChangeSearchInput = (event) => {
-    setSearchValue(event.target.value);
-  };
-  
+	const onAddToCart = (obj) => {
+		if (cartItems.find((item) => Number(item.id) === Number(obj.id))) {
+			axios.delete(
+				`https://634934c9a59874146b19fe11.mockapi.io/cart/${obj.id}`,
+			);
+			setCartItems((prev) =>
+				prev.filter((item) => Number(item.id) !== Number(obj.id)),
+			);
+		} else {
+			axios.post('https://634934c9a59874146b19fe11.mockapi.io/cart', obj);
+			setCartItems((prev) => [...prev, obj]);
+		}
+	};
 
-  return (
-    <div className="wrapper clear">
-      {cartOpened && <Sidebar items={cartItems} onClose={() => setCartOpened(false)} onRemove={onRemoveItem} />}
-      <Header onClickCart = {() => setCartOpened(true)} />
+	const onRemoveItem = (id) => {
+		axios.delete(`https://634934c9a59874146b19fe11.mockapi.io/cart/${id}`); //удаление данных в бэкенд в корзине
+		setCartItems((prev) => prev.filter((item) => item.id !== id));
+	};
 
-      <Route path="/" exact>
-        <Home 
-          items={items} 
-          searchValue={searchValue} 
-          setSearchValue={setSearchValue} 
-          onChangeSearchInput={onChangeSearchInput} 
-          onAddToFavorite={onAddToFavorite} 
-          onAddToCart={onAddToCart}
-        />
-      </Route>
+	const onAddToFavorite = async (obj) => {
+		try {
+			if (favorites.find((favObj) => favObj.id === obj.id)) {
+				axios.delete(
+					`https://634934c9a59874146b19fe11.mockapi.io/favorite/${obj.id}`,
+				);
+				setFavorites((prev) => prev.filter((item) => item.id !== obj.id));
+			} else {
+				const { data } = await axios.post(
+					'https://634934c9a59874146b19fe11.mockapi.io/favorite',
+					obj,
+				); //занесение данных в бэкенд в закладки
+				setFavorites((prev) => [...prev, data]);
+			}
+		} catch (error) {
+			alert('Не удалось добавить в закладки');
+		}
+	};
 
-      <Route path="/favorites" exact>
-        <Favorites items={favorites} onAddToFavorite={onAddToFavorite} />
-      </Route>
-    </div>
-  );
+	const onChangeSearchInput = (event) => {
+		setSearchValue(event.target.value);
+	};
+
+	return (
+		<div className='wrapper clear'>
+			{cartOpened && (
+				<Sidebar
+					items={cartItems}
+					onClose={() => setCartOpened(false)}
+					onRemove={onRemoveItem}
+				/>
+			)}
+			<Header onClickCart={() => setCartOpened(true)} />
+
+			<Route path='/' exact>
+				<Home
+					items={items}
+					cartItems={cartItems}
+					searchValue={searchValue}
+					setSearchValue={setSearchValue}
+					onChangeSearchInput={onChangeSearchInput}
+					onAddToFavorite={onAddToFavorite}
+					onAddToCart={onAddToCart}
+					isLoading={isLoading}
+				/>
+			</Route>
+
+			<Route path='/favorites' exact>
+				<Favorites items={favorites} onAddToFavorite={onAddToFavorite} />
+			</Route>
+		</div>
+	);
 }
 
 export default App;
